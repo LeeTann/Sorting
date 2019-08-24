4beb1a94030326a4277d450578d7fa22d048ce18
ltann <leeroyleeroy18@gmail.com>
2019-08-24 09:05:58 -0700

    Finished selection and bubbl sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,27 +1,38 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr) - 1): # loop through the array. start with current index. set it to smallest.
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        
+        for j in range(cur_index + 1, len(arr)): # loop through the rest of array and compare a second time called j
+            if arr[j] < arr[smallest_index]: # if actual value of j is less than the actual value of smallest index
+                smallest_index = j # set the index j to index of smallest value
 
         # TO-DO: swap
-
-
-
+        smallest_value = arr[smallest_index] # move actual value of smallest index to a temporary variable.
+        arr[smallest_index] = arr[cur_index] # move actual value of current index to actual value of smallest index
+        arr[cur_index]= smallest_value # move temporary value to actual value of current index
 
     return arr
 
+print(selection_sort(arr=[5, 2, 8, 4, 7, 1, 6]))
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort( bubble_arr ): 
+    for i in range(len(bubble_arr)): # loop through the array
 
-    return arr
+        for j in range(len(bubble_arr) - 1): # loop through the array a second time with j. minus len - 1
+            
+            if bubble_arr[j] > bubble_arr[j + 1]: # compare if 1st value > second value:
+                # Swap
+                temp = bubble_arr[j]
+                bubble_arr[j] = bubble_arr[j + 1]
+                bubble_arr[j + 1] = temp
+                
+    return bubble_arr
+
+print(bubble_sort(bubble_arr=[3, 1, 4, 5, 2, 9, 7, 6, 8, 10, 11]))
 
 
 # STRETCH: implement the Count Sort function below
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,10 +3,27 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+    # loop thru the first array, loop thru the second array. 
+    # compare if first array < second array. if so concatnate the arrays variable. 
     
+    temp_array = []
+
+    for i in range(len(arrA)): 
+      temp_array.append(arrA[i])
+
+      for j in range(len(arrB)):
+        if arrB[j] < arrA[i]:
+          temp_array.append(arrB[j])
+          print(temp_array)
+        elif arrB[j] > arrA[i]:
+          temp_array.append(arrA[i])
+          
+        
     return merged_arr
+    
+    
 
-
+merge([1, 2, 3, 4, 5], [10, 11, 12, 13, 14, 15, 16])
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
