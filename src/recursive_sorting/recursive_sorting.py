bd852ff2a3626d9bc2c7fe3a2edb60fc30b24780
ltann <leeroyleeroy18@gmail.com>
2019-08-25 11:51:21 -0700

    Recursion complete. Day 2 MVP done.

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,36 +1,60 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    print("num of elements", elements)
+    merged_arr = []
+    cur_index_A = 0
+    cur_index_B = 0
+
     # TO-DO
-    # loop thru the first array, loop thru the second array. 
-    # compare if first array < second array. if so concatnate the arrays variable. 
+
+    for i in range(elements):
+
+      # If no more items in arrA, then add items in arrB.
+      if cur_index_A >= len(arrA):
+        merged_arr.append(arrB[cur_index_B])
+        cur_index_B += 1
+
+      # If no more items in arrB, then add items in arrA.
+      elif cur_index_B >= len(arrB):
+        merged_arr.append(arrA[cur_index_A])
+        cur_index_A += 1
+      
+      # If A < B, add item A.
+      elif arrA[cur_index_A] < arrB[cur_index_B]:
+        merged_arr.append(arrA[cur_index_A])
+        cur_index_A += 1
+      
+      # else add B
+      else:
+        merged_arr.append(arrB[cur_index_B])
+        cur_index_B += 1
     
-    temp_array = []
-
-    for i in range(len(arrA)): 
-      temp_array.append(arrA[i])
-
-      for j in range(len(arrB)):
-        if arrB[j] < arrA[i]:
-          temp_array.append(arrB[j])
-          print(temp_array)
-        elif arrB[j] > arrA[i]:
-          temp_array.append(arrA[i])
-          
-        
     return merged_arr
-    
-    
 
-merge([1, 2, 3, 4, 5], [10, 11, 12, 13, 14, 15, 16])
+# print(merge([5, 7], [2, 6, 8, 9]))
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # Base case
+    if len(arr) == 1:
+      individual_arr = arr[0]
+      print("individual array: ", individual_arr)
+
+    # recurse case
+    else:
+      mid = len(arr) // 2
+      print("mid is", mid)
+      left = merge_sort(arr[:mid])
+      print("left is", left)
+      right = merge_sort(arr[mid:])
+      print("right is", right)
+      arr = merge(left, right)
 
     return arr
 
-
+print(merge_sort([7, 5, 1, 4, 2, 8]))
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
